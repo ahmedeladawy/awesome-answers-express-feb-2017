703d7d19c0f486c281980d90275671aeca7f6d53
ahmed <ahmedsaid5059@gmail.com>
2017-04-26 15:36:18 -0700

    Add answer list in Question#show

--- routes/questions.js
+++ routes/questions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const answers = require('./answers')
+const answers = require('./answers');
 
 const Question = require('../models/index').Question;
 // ð NEW! Destructuring
@@ -10,7 +10,7 @@ const Question = require('../models/index').Question;
 // Questions#index URL: /questions VERB: GET
 router.get('/', function (request, response, next) {
   Question
-    .findAll({order: [['createdAt', 'DESC'],['updatedAt', 'DESC']]}) // this returns promise...
+    .findAll({order: [['createdAt', 'DESC'], ['updatedAt', 'DESC']]}) // this returns promise...
     .then(function (questions) { // allowing us to use .then
       // the path of the template that response.render takes
       // is relative to the view/ folder by default
@@ -29,6 +29,7 @@ router.get('/new', function (req, res) {
   res.render('questions/new', {question: question});
 })
 
+// Questions#create URL: /questions VERB: POST
 router.post('/', function (req, res) {
   // .body is a property of the request object that
   // contains all form data as a JavaScript object
@@ -54,15 +55,38 @@ router.get('/:id', function (req, res) {
   Question
     .findById(id)
     .then(function (question) {
-      res.render('questions/show', {question: question})
-    });
+      return Promise.all([
+        question,
+        question.getAnswers({order: [['createdAt', 'DESC']]})
+      ])
+    })
+    // NEW! Array Destructuring
+    // const [first, second, ...rest] = [1, 2, 3, 4, 5, 6]
+    // first === 1; second === 2, rest === [3, 4, 5. 6]
+    // ð can also be done with function arguments ð
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    .then(function ([question, answers]) {
+      res.render('questions/show', {question: question, answers: answers})
+    })
 })
 
-//URL: /question/:questionId/answers VERB: All of them
-router.use('/:questionId/answers', answers)
+// URL: /questions/:questionId/answers VERB: All of them!
+router.use('/:questionId/answers', answers);
 
 module.exports = router;
 
+
+
+
+
+
+
+
+
+
+
+
+/* */
 
 
 
