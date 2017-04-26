e96d9b7f205c5a34dfb6847e1b881c157d7279d9
ahmed <ahmedsaid5059@gmail.com>
2017-04-26 09:30:44 -0700

    Add view page

--- routes/questions.js
+++ routes/questions.js
@@ -5,6 +5,7 @@ const Question = require('../models/index').Question;
 // ð NEW! Destructuring
 // const {Question} = require('../models/index');
 
+//Questions#index URL: /questions VERB: GET
 router.get('/', function (request, response, next) {
   Question
     .findAll() // this returns promise...
@@ -17,6 +18,18 @@ router.get('/', function (request, response, next) {
       // the rendered template as variables
     });
   // All Sequelize query methods return a promise
+})
+
+// Questions#show URL: /questions/:id VERB GET
+// for a url '/questions/99', the req.params object will be equal to {id: '99'}
+router.get('/:id', function(req, res){
+  // res.send(req.params);
+  const id = req.params.id;
+  Question 
+  .findById(id)
+  .then(function (question){
+    res.render('questions/show', {question: question})
+  })
 })
 
 module.exports = router;
